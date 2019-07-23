a2a87b1166de76ad0c43935d29b943dc8a1a30b0
qianjiu <qianjiu@email.com>
2019-07-23 17:05:50 +0800

    消息总线bus刷新配置文件

--- spring-cloud-config-client/src/main/java/com/xiangzi/springcloudconfigclient/ConfigClientApplication.java
+++ spring-cloud-config-client/src/main/java/com/xiangzi/springcloudconfigclient/ConfigClientApplication.java
@@ -3,10 +3,8 @@ package com.xiangzi.springcloudconfigclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @EnableDiscoveryClient   //激活配置中心的支持
-@RefreshScope
 @SpringBootApplication
 public class ConfigClientApplication {
 
